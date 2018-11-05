73e581e971784906de81dc4f8f58015ee5d76888
Anita <a.rybialek@gmail.com>
2018-11-05 11:18:04 +0100

    comments with values have been deleted

--- analiza_parametryczna.py
+++ analiza_parametryczna.py
@@ -23,7 +23,7 @@ def los1(frequency, length, base_height, subscriber_height, medium_height):
     + 12 * np.log10(base_height - medium_height)
     + 0.6 * np.log10(medium_height - subscriber_height)
     - 16.2 * np.log10( 0.5 * (base_height - subscriber_height) )  )
-    return np.round(result_los1, 2) #119.98224942273157
+    return np.round(result_los1, 2) 
 
 #NLOS1
 def nlos1(frequency, length, base_height, subscriber_height, medium_height):
@@ -32,14 +32,14 @@ def nlos1(frequency, length, base_height, subscriber_height, medium_height):
     - (47.2 * np.log10(base_height - medium_height))
     + (0.3 * np.log10(medium_height - subscriber_height))
     + (34.4 * np.log10( 0.5 * (base_height - subscriber_height) ) ) )
-    return np.round(result_nlos1, 2) #303.04692849058847
+    return np.round(result_nlos1, 2) 
 
 #LOS2
 def los2(frequency, length, base_height, subscriber_height, medium_height, coefficient):
     result_los2 = ( 23 + 20 * np.log10(frequency) + 16.57 * np.log10(length)
     + (22.1 * np.log10(base_height) - 10.3 * np.log10(subscriber_height) )
     + 8.45 * np.log10(base_height - medium_height) - 5.3 * np.log10(coefficient) )
-    return np.round(result_los2, 2) #118.6650244835433
+    return np.round(result_los2, 2) 
 
 #NLOS2
 def nlos2(frequency, length, base_height, subscriber_height, medium_height):
@@ -47,7 +47,7 @@ def nlos2(frequency, length, base_height, subscriber_height, medium_height):
     + ((-35 * np.log10(base_height)) + (16.6 * np.log10(subscriber_height))) 
     - (26.3 * np.log10(base_height - medium_height)) 
     + (23.9 * np.log10( 0.5 * (base_height - subscriber_height) ))  )
-    return np.round(result_nlos2, 2) #132.9846320870627
+    return np.round(result_nlos2, 2) 
 
 
 ###################################################### WYKRESY #######################################################################
--- arkusz_kalkulacyjny.py
+++ arkusz_kalkulacyjny.py
@@ -100,7 +100,7 @@ def los1(frequency, length, base_height, subscriber_height, medium_height):
     + 12 * np.log10(base_height - medium_height)
     + 0.6 * np.log10(medium_height - subscriber_height)
     - 16.2 * np.log10( 0.5 * (base_height - subscriber_height) )  )
-    return np.round(result_los1, 2) #119.98224942273157
+    return np.round(result_los1, 2) 
 
 ########################################################################################################################
 #NLOS1
@@ -110,7 +110,7 @@ def nlos1(frequency, length, base_height, subscriber_height, medium_height):
     - (47.2 * np.log10(base_height - medium_height))
     + (0.3 * np.log10(medium_height - subscriber_height))
     + (34.4 * np.log10( 0.5 * (base_height - subscriber_height) ) ) )
-    return np.round(result_nlos1, 2) #303.04692849058847
+    return np.round(result_nlos1, 2) 
 
 ########################################################################################################################
 #LOS2
@@ -118,7 +118,7 @@ def los2(frequency, length, base_height, subscriber_height, medium_height, coeff
     result_los2 = ( 23 + 20 * np.log10(frequency) + 16.57 * np.log10(length)
     + (22.1 * np.log10(base_height) - 10.3 * np.log10(subscriber_height) )
     + 8.45 * np.log10(base_height - medium_height) - 5.3 * np.log10(coefficient) )
-    return np.round(result_los2, 2) #118.6650244835433
+    return np.round(result_los2, 2) 
 
 ########################################################################################################################
 #NLOS2
@@ -127,7 +127,7 @@ def nlos2(frequency, length, base_height, subscriber_height, medium_height):
     + ((-35 * np.log10(base_height)) + (16.6 * np.log10(subscriber_height))) 
     - (26.3 * np.log10(base_height - medium_height)) 
     + (23.9 * np.log10( 0.5 * (base_height - subscriber_height) ))  )
-    return np.round(result_nlos2, 2) #132.9846320870627
+    return np.round(result_nlos2, 2) 
 
 
 ##################################################### MAIN PROGRAM ####################################################################
